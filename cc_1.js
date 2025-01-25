14c3acbc94e17dc4377f69f32e33b3226991728d
Derek Ruize <derekruiz@usf.edu>
2025-01-25 11:56:40 -0500

    Task 1 - Employee Information

---
+++ cc_1.js
@@ -0,0 +1,9 @@
+// Task 1
+let employeeName = "John Doe"; // Employee Name
+const employeeID = "12345"; // Employee ID
+var isActive = true; // Employees Activity
+
+console.log(typeof employeeName);
+console.log(typeof employeeID);
+console.log(typeof isActive);
+
